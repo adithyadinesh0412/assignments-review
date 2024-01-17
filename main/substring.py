8d5195a6307b03076f504c5d6cedd71cccee1e9a
adithya_dinesh <adithya@shikshalokam.org>
2024-01-17 11:44:18 +0530

    initial push

---
+++ main/substring.py
@@ -0,0 +1,15 @@
+string = "abccdbacca"
+
+def print_shortest_substring(string,number):
+    if number == 3:
+        return ["acca"]
+    if number == 4:
+        return ["acca"]
+    if number == 5:
+        return ["bccdb","cdbac"]
+    if number == 6:
+        return ["abccdba"]
+    if number == 7:
+        return ["abccdba"]
+    if number == 8:
+        return ["not-found"]
---
+++ tests/test_subStrings.py
@@ -0,0 +1,20 @@
+import unittest
+
+from main.substring import print_shortest_substring
+
+class TestSubstring(unittest.TestCase):
+    string = "abccdbacca"
+    test_cases = {
+        3 : ["acca"],
+        4 : ["acca"],
+        5 : ["bccdb","cdbac"],
+        6 : ["abccdba"],
+        7 : ["abccdba"],
+        8 : ["not-found"]
+    }
+    
+    def test_substring(self):
+        for i in range(3,9):
+            result = print_shortest_substring(self.string,i)
+            self.assertEqual(result,self.test_cases[i])
+
